b7e486d48946a8b45518b07d09158c30f08a8e3c
henrique <78827087+hewrique@users.noreply.github.com>
2022-03-16 18:32:56 -0300

    Add files via upload

---
+++ prototype/teste.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+/**
+ *
+ * @author gabri
+ */
+public class teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String login="";
+        String senha="";
+        String livros="";
+        String autores="";
+        String editora ="";
+        String área = "";
+        System.out.println("Primeiro registro:");
+        bibliotecario start = new bibliotecario(login, senha, livros, autores, editora, área);
+        start.clonar();
+        start.registrar();
+        
+    }
+    
+}
